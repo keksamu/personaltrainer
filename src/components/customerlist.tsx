58af6d5739f67743bc1a184326e0c18c5836d624
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2025-11-24 13:16:41 +0200

    Export functionality

--- src/components/customerlist.tsx
+++ src/components/customerlist.tsx
@@ -5,22 +5,24 @@ import type { Tcustomer } from "../types";
 import { Button } from "@mui/material";
 import AddCustomer from "./addcustomer";
 import EditCustomer from "./editcustomer";
+import Papa from 'papaparse';
 
 export default function CustomerList() {
     const [customers, setCustomers] = useState<Tcustomer[]>([]);
 
     const columns: GridColDef[] = [
-        { field: 'firstname', headerName: 'First Name', width: 150 },
-        { field: 'lastname', headerName: 'Last Name', width: 150 },
-        { field: 'email', headerName: 'Email', width: 200 },
-        { field: 'phone', headerName: 'Phone', width: 150 },
-        { field: 'streetaddress', headerName: 'Address', width: 200 },
+        { field: 'firstname', headerName: 'First Name', width: 130 },
+        { field: 'lastname', headerName: 'Last Name', width: 130 },
+        { field: 'email', headerName: 'Email', width: 180 },
+        { field: 'phone', headerName: 'Phone', width: 130 },
+        { field: 'streetaddress', headerName: 'Address', width: 180 },
         { field: 'postcode', headerName: 'Postcode', width: 100 },
-        { field: 'city', headerName: 'City', width: 150 },
-        { field: 'actions', type: 'actions', width: 150,
+        { field: 'city', headerName: 'City', width: 120 },
+        { field: 'actions', type: 'actions', headerName: 'Actions', width: 250,
             getActions: (params: GridRowParams) => [
                 <EditCustomer customer={params.row} handleEdit={handleEdit} />,
-                <Button size="small" color="error" onClick={() => { handleDelete(params.row._links.self.href)}}>DELETE</Button>
+                <Button size="small" color="error" onClick={() => { handleDelete(params.row._links.self.href)}}>DELETE</Button>,
+                <Button size="small" color="primary" onClick={() => handleExport(params.row)}>EXPORT</Button>
             ]
         }
     ];
@@ -88,6 +90,30 @@ export default function CustomerList() {
         } catch (err) {
             console.log(err);
         }
+    };
+
+    const handleExport = (customer: Tcustomer) => {
+        const customerData = {
+            'First Name': customer.firstname,
+            'Last Name': customer.lastname,
+            'Email': customer.email,
+            'Phone': customer.phone,
+            'Street Address': customer.streetaddress,
+            'Postcode': customer.postcode,
+            'City': customer.city
+        };
+
+        const csv = Papa.unparse([customerData]);
+        const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+        const link = document.createElement('a');
+        const url = URL.createObjectURL(blob);
+        
+        link.setAttribute('href', url);
+        link.setAttribute('download', `${customer.firstname}_${customer.lastname}_customer_data.csv`);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     };
 
     useEffect(() => { getCustomers(); }, []);
